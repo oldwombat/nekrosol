091f99081d80fccbcda8d44f5335ea822a2e525c
oldwombat <13641602+oldwombat@users.noreply.github.com>
2026-02-21 23:40:26 +1100

    fix pluralisation of Lore

--- backend/src/collections/Lore.ts
+++ backend/src/collections/Lore.ts
@@ -2,6 +2,10 @@ import type { CollectionConfig } from 'payload'
 
 export const Lore: CollectionConfig = {
   slug: 'lore',
+  labels: {
+    singular: 'Lore',
+    plural: 'Lore',
+  },
   access: {
     read: () => true,
   },
